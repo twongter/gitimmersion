013b44274cf736409759926fc4cfbb13e415fd99
Terry Wong <twongter@gmail.com>
2016-05-08 13:37:01 +0000

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,4 @@
-# Default is World
-# Author: Jim Weirich (jim@somewhere.com)
-name = ARGV.first || "World"
-puts "Hello, #{name}!"
+puts "What's your name"
+my_name = gets.strip
+
+puts "Hello, #{my_name}!"
